aedf284db0b315b2eff945e4a811ebee3f37123f
Draska <maxemijo@gmail.com>
2018-11-20 10:15:24 -0300

    using postgres to deploy on heroku

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -20,8 +22,11 @@ type Handler struct {
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	dbURL := os.Getenv("DATABASE_URL")
 	var h Handler
-	db, err := gorm.Open("sqlite3", "jokers.db")
+	// db, err := gorm.Open("sqlite3", "jokers.db")
+	db, err := gorm.Open("postgres", dbURL)
 	defer db.Close()
 	if err != nil {
 		log.Fatal("unable to connect to DB", err)
@@ -33,10 +38,29 @@ func main() {
 	r.HandleFunc("/add/{joker}", h.addJoker).Methods("GET")
 	r.HandleFunc("/joke/{joker}", h.addToJoker).Methods("GET")
 	r.HandleFunc("/score", h.score).Methods("GET")
+	r.HandleFunc("/score", h.addMany).Methods("POST")
 	fs := http.FileServer(http.Dir("./public"))
 	r.PathPrefix("/").Handler(fs)
 
-	log.Fatal(http.ListenAndServe("localhost:15000", r))
+	log.Fatal(http.ListenAndServe(port, r))
+}
+
+func (h Handler) addMany(w http.ResponseWriter, r *http.Request) {
+	var jokers []Joker
+	err := json.NewDecoder(r.Body).Decode(&jokers)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	for _, joker := range jokers {
+		var jk Joker
+		if err = h.db.Where("name=?", joker.Name).First(&jk).Error; gorm.IsRecordNotFoundError(err) {
+			h.db.Create(&joker)
+		} else {
+			jk.Jokes = joker.Jokes
+			h.db.Save(&jk)
+		}
+	}
 }
 
 func (h Handler) addToJoker(w http.ResponseWriter, r *http.Request) {
@@ -44,7 +68,7 @@ func (h Handler) addToJoker(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	name := vars["joker"]
 	h.db.Where("name=?", name).First(&joker)
-	joker.Jokes += 1
+	joker.Jokes++
 	h.db.Save(&joker)
 	err := json.NewEncoder(w).Encode(joker)
 	if err != nil {
